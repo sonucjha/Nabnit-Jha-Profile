359715f195a10d5641b712ca46e93d3c4e323bf7
sonu <sonucjha@gmail.com>
2024-11-22 11:56:35 +0530

    improve code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+            ],
             refresh: true,
         }),
     ],
