f90073ddcd3d652b6c786c691d22ac257cc2fe49
sonu <sonucjha@gmail.com>
2024-11-23 20:45:45 +0530

    front end html

---
+++ resources/views/layouts/nabnit.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Nabnit-Jha-Profile')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- preloader-start -->
+    <div id="preloader">
+        <div class="rasalina-spin-box"></div>
+    </div>
+    <!-- preloader-end -->
+
+    <!-- Scroll-top -->
+    <button class="scroll-top scroll-to-target" data-target="html">
+        <i class="fas fa-angle-up"></i>
+    </button>
+    <!-- Scroll-top-end-->
+
+    <!-- header area -->
+    @include('nabnit_partials.header')
+
+    <!-- main area -->
+    @include('nabnit_partials.main_area')
+
+    <!-- footer area -->
+    @include('nabnit_partials.footer')
+
+    <script src="{{ asset('css/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
