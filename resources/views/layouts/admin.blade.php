8b1491b05a453a54c7be334fdb400f53c7b1bf95
sonu <sonucjha@gmail.com>
2024-11-24 22:59:29 +0530

    improve code

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard')</title>
 
-     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('css/admin/app.css') }}"> <!-- Add custom styles here -->
     <link rel="stylesheet" href="{{ asset('css/admin/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}">
 </head>
+
 <body>
     <div class="d-flex" style="height: 100vh;">
         <!-- Sidebar -->
         @include('partials.sidebar')
-        
+
         <!-- Main Content -->
         <div class="flex-grow-1 d-flex flex-column">
             <!-- Header -->
             @include('partials.header')
-            
+
             <!-- Page Content -->
             <main class="flex-grow-1 p-3">
                 @yield('content')
@@ -31,5 +33,5 @@
 
     <script src="{{ asset('css/bootstrap.bundle.min.js') }}"></script>
 </body>
-</html>
 
+</html>
