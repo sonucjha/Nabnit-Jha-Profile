09247a77ed53d207c1a7fbaa09f8929bd9da70df
sonu <sonucjha@gmail.com>
2024-11-24 11:44:16 +0530

    improve code

--- resources/views/layouts/nabnit.blade.php
+++ resources/views/layouts/nabnit.blade.php
@@ -2,41 +2,62 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Nabnit-Jha-Profile')</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
 
+    <title>Nabnit-Jha-Profile</title>
+    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <!-- Additional CSS Files -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <!-- Add custom styles here -->
-    <link rel="stylesheet" href="{{ asset('css/nabnit/nabnit_header.css') }}"> 
+
+    {{-- <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/templatemo-style.css" />
+    <link rel="stylesheet" href="assets/css/owl.css" />
+    <link rel="stylesheet" href="assets/css/lightbox.css" /> --}}
+
 </head>
 
 <body>
-    <!-- preloader-start -->
-    <div id="preloader">
-        <div class="rasalina-spin-box"></div>
+    <div id="page-wraper">
+        <!-- Sidebar Menu -->
+        @include('nabnit.sidebar')
+        <!-- End Sidebar Menu -->
+
+        <!-- About Me Menu -->
+        @include('nabnit.about_me')
+        <!-- End Aboutt Me Menu -->
+
+        <!-- My Services Menu -->
+        @include('nabnit.my_services')
+        <!-- End My Services Menu -->
+
+        <!-- My Work Menu -->
+        @include('nabnit.my_work')
+        <!-- End My Work Menu -->
+
+        <!-- Contact Me Menu -->
+        @include('nabnit.contact_me')
+        <!-- End Contact Me Menu -->
+
+
     </div>
-    <!-- preloader-end -->
 
-    <!-- Scroll-top -->
-    <button class="scroll-top scroll-to-target" data-target="html">
-        <i class="fas fa-angle-up"></i>
-    </button>
-    <!-- Scroll-top-end-->
-
-    <!-- header area -->
-    @include('nabnit_partials.header')
-
-    <!-- main area -->
-    @include('nabnit_partials.main_area')
-
-    <!-- footer area -->
-    @include('nabnit_partials.footer')
-
-    
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('css/jquery.min.js') }}"></script>
     <script src="{{ asset('css/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('css/nabnit/nabnit_header.js') }}"></script>
+
+    {{-- <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/custom.js"></script> --}}
+
 </body>
 
 </html>
---
+++ resources/views/nabnit_partials/sidebar.blade.php
@@ -0,0 +1,49 @@
+      <!-- Sidebar Menu -->
+      <div class="responsive-nav">
+        <i class="fa fa-bars" id="menu-toggle"></i>
+        <div id="menu" class="menu">
+          <i class="fa fa-times" id="menu-close"></i>
+          <div class="container">
+            <div class="image">
+              <a href="#"><img src="assets/images/author-image.jpg" alt="" /></a>
+            </div>
+            <div class="author-content">
+              <h4>Reflux Me</h4>
+              <span>Web Designer</span>
+            </div>
+            <nav class="main-nav" role="navigation">
+              <ul class="main-menu">
+                <li><a href="#section1">About Me</a></li>
+                <li><a href="#section2">What I’m good at</a></li>
+                <li><a href="#section3">My Work</a></li>
+                <li><a href="#section4">Contact Me</a></li>
+              </ul>
+            </nav>
+            <div class="social-network">
+              <ul class="soial-icons">
+                <li>
+                  <a href="https://fb.com/templatemo"
+                    ><i class="fa fa-facebook"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-dribbble"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-rss"></i></a>
+                </li>
+              </ul>
+            </div>
+            <div class="copyright-text">
+              <p>Copyright 2019 Reflux Design</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Sidebar Menu -->
