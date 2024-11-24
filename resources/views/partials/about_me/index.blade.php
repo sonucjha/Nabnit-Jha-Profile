d203de7be3f500f5f79b599231435df64812607f
sonu <sonucjha@gmail.com>
2024-11-25 00:02:36 +0530

    improve code

--- resources/views/partials/about_me/index.blade.php
+++ resources/views/partials/about_me/index.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.admin')
 
 @section('content')
-<div>
-    <h1>About Me Details</h1>
-    <a href="{{ route('about_me.create') }}">Add New</a>
-    <ul>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">About Me Details</h1>
+    <a href="{{ route('about_me.create') }}" class="btn btn-primary mb-3">Add New</a>
+    <div class="list-group">
         @foreach ($aboutMe as $entry)
-        <li>
-            <h3>{{ $entry->title }}</h3>
-            <p>{{ $entry->description }}</p>
-            <a href="{{ route('about_me.edit', $entry->id) }}">Edit</a>
+        <div class="list-group-item">
+            <h3 class="mb-2">{{ $entry->title }}</h3>
+            <p class="mb-3">{{ $entry->description }}</p>
+            <a href="{{ route('about_me.edit', $entry->id) }}" class="btn btn-warning btn-sm mr-2">Edit</a>
             <form action="{{ route('about_me.destroy', $entry->id) }}" method="POST" style="display: inline;">
                 @csrf
                 @method('DELETE')
-                <button type="submit">Delete</button>
+                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
             </form>
-        </li>
+        </div>
         @endforeach
-    </ul>
+    </div>
 </div>
 @endsection
