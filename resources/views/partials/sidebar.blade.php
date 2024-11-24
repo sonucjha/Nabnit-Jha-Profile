51615ff952cfa6f872e9db436e8c6b883eec1b49
sonu <sonucjha@gmail.com>
2024-11-24 18:12:09 +0530

    improve code

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,27 +1,28 @@
 <div class="bg-dark text-white p-3" style="width: 250px;">
-    <h5 class="text-center">Admin Menu</h5>
-    <ul class="nav flex-column">
+    {{-- <h5 class="text-center">Admin Menu</h5> --}}
+    <ul class="nav flex-column" style="margin-top: 3rem;">
         <!-- Dashboard -->
         <li class="nav-item">
             <a class="nav-link text-white d-flex align-items-center" href="#">
-                <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                <i class="fas fa-tachometer-alt me-2"></i> About Me
             </a>
         </li>
-        <!-- Users Dropdown -->
-        <li class="nav-item dropdown">
-            <a class="nav-link text-white d-flex align-items-center dropdown-toggle" href="#" id="userMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-users me-2"></i> Users
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="userMenu">
-                <li><a class="dropdown-item" href="#">All Users</a></li>
-                <li><a class="dropdown-item" href="#">Add User</a></li>
-                <li><a class="dropdown-item" href="#">User Roles</a></li>
-            </ul>
-        </li>
-        <!-- Settings -->
+        <!-- What I’m good at -->
         <li class="nav-item">
             <a class="nav-link text-white d-flex align-items-center" href="#">
-                <i class="fas fa-cog me-2"></i> Settings
+                <i class="fas fa-tachometer-alt me-2"></i> What I’m good at
+            </a>
+        </li>
+        <!-- My Work -->
+        <li class="nav-item">
+            <a class="nav-link text-white d-flex align-items-center" href="#">
+                <i class="fas fa-tachometer-alt me-2"></i> My Work
+            </a>
+        </li>
+        <!-- Image Upload -->
+        <li class="nav-item">
+            <a class="nav-link text-white d-flex align-items-center" href="#">
+                <i class="fas fa-cog me-2"></i> Image Upload
             </a>
         </li>
     </ul>
