840d43952ad1ec8e69dd487b67d6ee2e32f31e03
sonu <sonucjha@gmail.com>
2024-11-24 20:08:15 +0530

    improve code

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -20,6 +20,11 @@
             </a>
         </li>
         <li class="nav-item">
+            <a class="nav-link" href="{{ route('socical-account') }}">
+                <i class="fas fa-briefcase me-2"></i> Socical Account
+            </a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="{{ route('image_upload') }}">
                 <i class="fas fa-upload me-2"></i> Image Upload
             </a>
