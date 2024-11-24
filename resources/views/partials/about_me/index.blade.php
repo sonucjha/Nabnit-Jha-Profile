252764e59f581796ef179e0cef5985ef9c9bcbeb
sonu <sonucjha@gmail.com>
2024-11-25 00:09:08 +0530

    improve code

--- resources/views/partials/about_me/index.blade.php
+++ resources/views/partials/about_me/index.blade.php
@@ -1,22 +1,36 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container mt-5">
-    <h1 class="text-center mb-4">About Me Details</h1>
-    <a href="{{ route('about_me.create') }}" class="btn btn-primary mb-3">Add New</a>
-    <div class="list-group">
-        @foreach ($aboutMe as $entry)
-        <div class="list-group-item">
-            <h3 class="mb-2">{{ $entry->title }}</h3>
-            <p class="mb-3">{{ $entry->description }}</p>
-            <a href="{{ route('about_me.edit', $entry->id) }}" class="btn btn-warning btn-sm mr-2">Edit</a>
-            <form action="{{ route('about_me.destroy', $entry->id) }}" method="POST" style="display: inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
-            </form>
-        </div>
-        @endforeach
+<div class="container mt-4">
+    <h1>About Me Details</h1>
+    <div class="text-right mb-3">
+        <a href="{{ route('about_me.create') }}" class="btn btn-primary">Add New</a>
     </div>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col" class="text-center">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($aboutMe as $entry)
+            <tr>
+                <td>{{ $entry->title }}</td>
+                <td>{{ $entry->description }}</td>
+                <td class="text-center">
+                    <a href="{{ route('about_me.edit', $entry->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                    <form action="{{ route('about_me.destroy', $entry->id) }}" method="POST" style="display: inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this entry?')">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
