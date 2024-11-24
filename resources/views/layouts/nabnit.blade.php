6cbeb03b16458bfafcd707589ba90103bac2ae35
sonu <sonucjha@gmail.com>
2024-11-24 12:10:52 +0530

    improve code

--- resources/views/layouts/nabnit.blade.php
+++ resources/views/layouts/nabnit.blade.php
@@ -15,33 +15,33 @@
     <!-- Additional CSS Files -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
-    {{-- <link rel="stylesheet" href="assets/css/fontawesome.css" />
-    <link rel="stylesheet" href="assets/css/templatemo-style.css" />
-    <link rel="stylesheet" href="assets/css/owl.css" />
-    <link rel="stylesheet" href="assets/css/lightbox.css" /> --}}
+    <link rel="stylesheet" href="{{asset('css/assets/css/fontawesome.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/assets/css/templatemo-style.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/assets/css/owl.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/assets/css/lightbox.css')}}" />
 
 </head>
 
 <body>
     <div id="page-wraper">
         <!-- Sidebar Menu -->
-        @include('nabnit.sidebar')
+        @include('nabnit_partials.sidebar')
         <!-- End Sidebar Menu -->
 
         <!-- About Me Menu -->
-        @include('nabnit.about_me')
+        @include('nabnit_partials.about_me')
         <!-- End Aboutt Me Menu -->
 
         <!-- My Services Menu -->
-        @include('nabnit.my_services')
+        @include('nabnit_partials.my_services')
         <!-- End My Services Menu -->
 
         <!-- My Work Menu -->
-        @include('nabnit.my_work')
+        @include('nabnit_partials.my_work')
         <!-- End My Work Menu -->
 
         <!-- Contact Me Menu -->
-        @include('nabnit.contact_me')
+        @include('nabnit_partials.contact_me')
         <!-- End Contact Me Menu -->
 
 
@@ -53,10 +53,10 @@
     <script src="{{ asset('css/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('css/nabnit/nabnit_header.js') }}"></script>
 
-    {{-- <script src="assets/js/isotope.min.js"></script>
-    <script src="assets/js/owl-carousel.js"></script>
-    <script src="assets/js/lightbox.js"></script>
-    <script src="assets/js/custom.js"></script> --}}
+    <script src="{{asset('css/assets/js/isotope.min.js')}}"></script>
+    <script src="{{asset('css/assets/js/owl-carousel.js')}}"></script>
+    <script src="{{asset('css/assets/js/lightbox.js')}}"></script>
+    <script src="{{asset('css/assets/js/custom.js')}}"></script>
 
 </body>
 
