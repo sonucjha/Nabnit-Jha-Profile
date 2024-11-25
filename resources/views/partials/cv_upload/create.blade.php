238030e607174102b9969834e854601679a5701c
sonu <sonucjha@gmail.com>
2024-11-26 02:37:07 +0530

    improve code

--- resources/views/partials/cv_upload/create.blade.php
+++ resources/views/partials/cv_upload/create.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Upload CV</h1>
-    <form action="{{ route('cv_uploads.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="text" name="name" placeholder="Your Name">
-        <input type="email" name="email" placeholder="Your Email">
-        <input type="file" name="cv_file">
-        <button type="submit">Upload</button>
-    </form>
+    <div class="container">
+        <h1 class="my-4">Upload CV</h1>
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('cv_uploads.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Your Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Your Email</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cv_file" class="form-label">Upload CV</label>
+                        <input type="file" name="cv_file" id="cv_file" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/partials/cv_upload/edit.blade.php
+++ resources/views/partials/cv_upload/edit.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Edit CV</h1>
-    <form action="{{ route('cv_uploads.update', $cvUpload->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <input type="text" name="name" value="{{ $cvUpload->name }}">
-        <input type="email" name="email" value="{{ $cvUpload->email }}">
-        <input type="file" name="cv_file">
-        <button type="submit">Update</button>
-    </form>
+    <div class="container mt-4">
+        <h1 class="mb-4">Edit CV</h1>
+        <form action="{{ route('cv_uploads.update', $cvUpload->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" id="name" name="name" value="{{ $cvUpload->name }}" class="form-control">
+            </div>
+            
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" id="email" name="email" value="{{ $cvUpload->email }}" class="form-control">
+            </div>
+            
+            <div class="mb-3">
+                <label for="cv_file" class="form-label">Upload CV</label>
+                <input type="file" id="cv_file" name="cv_file" class="form-control">
+            </div>
+            
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>
+        </form>
+    </div>
 @endsection
