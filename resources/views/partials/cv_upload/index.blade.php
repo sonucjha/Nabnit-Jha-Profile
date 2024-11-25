0a8b370e572b5cf36a36d9c5b20fd8d0d9e403ff
sonu <sonucjha@gmail.com>
2024-11-26 02:27:01 +0530

    improve code

--- resources/views/partials/cv_upload/index.blade.php
+++ resources/views/partials/cv_upload/index.blade.php
@@ -2,13 +2,34 @@
 
 @section('content')
     <h1>CV Uploads</h1>
-    <a href="{{ route('cv_uploads.create') }}">Upload New CV</a>
-    <ul>
-        @foreach($cvUploads as $cv)
-            <li>
-                {{ $cv->name }} - <a href="{{ asset('storage/' . $cv->cv_file) }}" target="_blank">Download</a>
-                <a href="{{ route('cv_uploads.edit', $cv->id) }}">Edit</a>
-            </li>
-        @endforeach
-    </ul>
+    <a href="{{ route('cv_uploads.create') }}" class="btn btn-primary mb-3">Upload New CV</a>
+    <table class="table table-striped table-bordered">
+        <thead class="thead-dark">
+            <tr>
+                <th>CV File</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($cvUploads as $index => $cv)
+                <tr>
+                    <td>
+                        <a href="{{ asset('storage/' . $cv->cv_file) }}" target="_blank" class="btn btn-sm btn-info">
+                            Download
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('cv_uploads.edit', $cv->id) }}" class="btn btn-sm btn-warning">
+                            Edit
+                        </a>
+                        <form action="{{ route('cv_uploads.destroy', $cv->id) }}" method="POST" style="display: inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this CV?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
